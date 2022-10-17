a05aa9fd454a50962efd8a540df742912023d056
MrMendez <hirammendez000@gmail.com>
2022-10-17 09:50:37 -0500

    eliminar todos added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import { saludar } from './js/componentes';
 import './styles.css';
 
-const nombre = 'Fernando';
+import { Todo, TodoList } from './classes'
+import { crearTodoHtml } from './js/componentes';
 
-saludar( nombre );
+
+export const todoList = new TodoList();
+
+const tarea = new Todo('aprender js!!!');
+
+todoList.nuevoTodo( tarea );
+console.log( todoList );
+
+crearTodoHtml( tarea );
