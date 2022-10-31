5cb19638fdf92b5b08545a39b740d23f74690801
MrMendez <hirammendez000@gmail.com>
2022-10-31 09:50:10 -0600

    filters added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Todo, TodoList } from './classes'
 import { crearTodoHtml } from './js/componentes';
 
 
+
 export const todoList = new TodoList();
 
 const tarea = new Todo('aprender js!!!');
